9c183d2ee85cb871ede34608f5d481c1eae3aa74
Linda MacDonald <lindamacdonalde@gmail.com>
2023-09-09 10:11:41 +0200

    Initial commit

---
+++ src/pages/_app.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "styles/global.css";
+import Navbar from "components/Navbar";
+import { AuthProvider } from "util/auth";
+import { QueryClientProvider } from "util/db";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <QueryClientProvider>
+      <AuthProvider>
+        <>
+          <Navbar />
+
+          <Component {...pageProps} />
+        </>
+      </AuthProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default MyApp;
